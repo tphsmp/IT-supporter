082b2dcaef60c6cec016938b8b06ded1e228b0f8
tphsmp <40296154+tphsmp@users.noreply.github.com>
2022-07-29 14:08:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
-import logging
-from typing import Text
-
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
-    InlineKeyboardButton
 
-from config import TOKEN
+from config import *
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
+"""Обработка команды /start"""
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
-    await  bot.send_message(message.from_user.id,
-                            "Здравствуйте {0.first_name} {0.last_name}! Нажмите на кнопку меню и выберите /about "
-                            "\n Если среди предложенных вариантов нет пункта по вашему вопросу свяжитесь со службой "
-                            "поддержки".format(message.from_user))
+    await bot.send_message(message.from_user.id, GREETING_MESSAGE.format(message.from_user))
 
-
+"""Вывод списка команд по группам"""
 @dp.message_handler(commands=['about'])
 async def bot_message(message: types.Message):
     await bot.send_message(message.from_user.id, "Выберите соответствующий вашему вопросу пункт из списка ниже: \n\n"
                                                  "/access - доступ к информационным системам и ресурсам компании \n\n"
                                                  "/software - установка, обновление программного обеспечения\n\n"
-                                                 "/ecp - получение обновление ЭЦП, доступ на порталы egov, ЭСФ и прочие порталы с авторизацией по ЭЦП \n\n"
-                                                 "/equipment - установка перефирийного оборудования (принтеры. сканеры, камеры и т.д.) \n\n"
+                                                 "/ecp - получение обновление ЭЦП, доступ на порталы egov, ЭСФ и "
+                                                 "прочие порталы с авторизацией по ЭЦП \n\n"
+                                                 "/equipment - установка перефирийного оборудования "
+                                                 "(принтеры. сканеры, камеры и т.д.) \n\n"
                                                  "/sap - вопросы по SAP \n\n"
                                                  "/1C - вопрос по 1С\n\n"
                                                  "/network - локальная сеть, телефония, доступ в интернет \n\n"
                                                  "/another - другой вопрос \n\n")
 
-
+"""Обработка команды /access"""
 @dp.message_handler(commands=['access'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -45,7 +39,7 @@ async def command_access(message: types.Message):
     keyboard.add(button_4)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /software"""
 @dp.message_handler(commands=['software'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -55,7 +49,7 @@ async def command_access(message: types.Message):
     keyboard.add(button_2)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /ecp"""
 @dp.message_handler(commands=['ecp'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -67,7 +61,7 @@ async def command_access(message: types.Message):
     keyboard.add(button_3)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /equipment"""
 @dp.message_handler(commands=['equipment'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -81,7 +75,7 @@ async def command_access(message: types.Message):
     keyboard.add(button_4)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /sap"""
 @dp.message_handler(commands=['sap'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -93,7 +87,7 @@ async def command_access(message: types.Message):
     keyboard.add(button_3)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /1C"""
 @dp.message_handler(commands=['1C'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -105,7 +99,7 @@ async def command_access(message: types.Message):
     keyboard.add(button_3)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /network"""
 @dp.message_handler(commands=['network'])
 async def command_access(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -119,9 +113,9 @@ async def command_access(message: types.Message):
     keyboard.add(button_4)
     await message.answer("Выберите действие", reply_markup=keyboard)
 
-
+"""Обработка команды /another"""
 @dp.message_handler(commands=['another'])
-async def command_access(message: types.Message):
+async def access(message: types.Message):
     await message.answer("Свяжитесь с специалистом сектора поддержки по номеру 5555")
 
 
@@ -129,15 +123,21 @@ async def command_access(message: types.Message):
 #     if (message.text == "ИС Оригами"):
 #         await message.answer("пароль сбросить да?")
 #
+
+"""Обработка кнопок раздела sap"""
 @dp.message_handler(lambda message: message.text == "Работа с программой" or message.text == "Другие вопросы")
 async def sono(message: types.Message):
     await message.answer("Свяжитесь со специалистом сектора безнес-приложений по номеру 2124 или 2156")
 
+"""Конец обработка кнопок раздела sap"""
 
+"""Обработка кнопок раздела 1C"""
 @dp.message_handler(lambda message: message.text != "Работа с программой" or message.text != "Другие вопросы")
 async def sono(message: types.Message):
     await message.answer("Свяжитесь со специалистом сектора поддержки по номеру 5555")
 
+"""Конец обработка кнопок раздела 1C"""
 
+"""Запуск бота"""
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
